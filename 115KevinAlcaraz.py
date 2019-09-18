6ed3b07331179ae94084a575e72a08280ee95c1e
kevin-alcaraz <55501099+kevin-alcaraz@users.noreply.github.com>
2019-09-18 11:13:44 -0400

    initial upload
    um idk

---
+++ 115KevinAlcaraz.py
@@ -0,0 +1,19 @@
+#   a115_buggy_image.py
+import turtle as trtl
+
+drawer = trtl.Turtle()
+drawer.pensize(40)
+drawer.circle(20)
+sticks = 6
+length = 70
+z = 380 / sticks
+drawer.pensize(5)
+n = 0
+while (n < sticks):
+  drawer.goto(0,0)
+  drawer.setheading(z*n)
+  drawer.forward(length)
+  n = n + 1
+drawer.hideturtle()'''
+wn = trtl.Screen()
+wn.mainloop()
